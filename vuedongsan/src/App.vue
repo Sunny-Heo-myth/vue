4b724dc185579068e9751aeecf0d0d56188aed4e
Alan <alan@g1project.net>
2023-08-31 15:34:11 +0900

    study: event handling

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,19 +1,63 @@
+
+<!-- PLAIN TEXT -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>one room shop</h2>
+
+  <div class="menu">
+    <a v-for="a in menu" :key=a>{{a}}</a>
+  </div>
+
+  <div class="rooms">
+    <h4 v-for="(product, i) in products" :key="i" :style="roomColor[i]">{{product}}
+      <p>{{ price[i] }} $</p>
+      <p><button @click="roomReportCnt[i]++">report</button> <span>report count: {{ roomReportCnt[i] }}</span></p>
+      <p><button @mouseover="countLike(i)">like</button> <span>like coount: {{ roomLikeCnt[i] }}</span></p>
+      <p>---------------------------------------</p>
+    </h4>
+  </div>
+
+  <div class="like">
+    <h4>likeyyy</h4>
+    
+  </div>
+
 </template>
 
+
+
+<!-- JAVASCRIPT -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      // all data goes in here
+      menu: ['home', 'shop', 'about'],
+      room: ['room1', 'room2', 'room3'],
+      products: ['Yeoksam', 'Cheonho', 'Mapo'],
+      price: [60, 70, 80],
+      roomColor: ['color: red', 'color: blue', 'color: green'],
+      roomReportCnt: [0, 0, 0],
+      roomLikeCnt: [0, 0, 0]
+    }
+  },
+  methods: {
+    countLike(i) {
+      this.roomLikeCnt[i]++;
+      console.log(this.roomLikeCnt[i]);
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
+
 </script>
 
+
+
+<!-- STYLE -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -21,6 +65,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+ color: white;
+ padding: 10px;
+}
+
 </style>
